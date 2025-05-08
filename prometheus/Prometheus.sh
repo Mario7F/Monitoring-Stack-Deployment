a279c1a1075aba9d1d2bca319584506331fc6272
Mario Fletcher <mariojfletcher@gmail.com>
2025-05-08 14:49:55 -0500

    Update Prometheus.sh

--- prometheus/Prometheus.sh
+++ prometheus/Prometheus.sh
@@ -8,3 +8,5 @@ sudo systemctl enable influxdb
 sudo dnf install -y http://mirror.linuxtrainingacademy.com/grafana/telegraf-1.15.3-1.x86_64.rpm
 sudo systemctl start telegraf
 sudo systemctl enable telegraf
+
+echo "Access Prometheus at :9090"
