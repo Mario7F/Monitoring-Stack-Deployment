e0f90246b2952138a76a623914c433ef19cb8cc6
Mario Fletcher <mariojfletcher@gmail.com>
2025-05-08 14:47:48 -0500

    Update Prometheus.sh

--- prometheus/Prometheus.sh
+++ prometheus/Prometheus.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo” Prometheus Files”
+echo” Prometheus Server”
 
+sudo dnf install -y http://mirror.linuxtrainingacademy.com/grafana/influxdb-1.8.2.x86_64.rpm
+sudo systemctl start influxdb
+sudo systemctl enable influxdb
+sudo dnf install -y http://mirror.linuxtrainingacademy.com/grafana/telegraf-1.15.3-1.x86_64.rpm
+sudo systemctl start telegraf
+sudo systemctl enable telegraf
