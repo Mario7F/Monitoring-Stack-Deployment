de2b03b994b3fa07505f17183e2c06f27b0f8e35
Mario Fletcher <mariojfletcher@gmail.com>
2025-05-08 14:47:19 -0500

    Update grafana.sh

--- grafana/grafana.sh
+++ grafana/grafana.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-echo” Grafana Files”
-
+echo "Install Grafana Server”
+sudo dnf install -y http://mirror.linuxtrainingacademy.com/grafana/grafana-7.2.0-1.x86_64.rpm
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server
